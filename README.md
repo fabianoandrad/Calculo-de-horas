bb3b5d49d7e66cc3a0ecdaa6ce2197c527606056
fabianoandrad <affatecnologia@gmail.com>
2021-02-23 10:21:17 -0300

    informação para rodar a aplicação

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Calculo-de-horas
 Cálculo de horas noturnas e diurnas
+ Para rodar a aplicação digite no terminal:
+
+ yarn webpack serve --mode development
--- src/App.js
+++ src/App.js
@@ -22,7 +22,20 @@ function App() {
     const valueMinOut = document.getElementById('minOut').value
 
     
+// Tratativas de erros
+if(valueHourIn == '' || valueMinIn == '' || valueHourOut == '' || valueMinOut == ''){
+  return alert('Todos os campos são obrigatórios!')
+  
+}else if(valueHourIn > 24 || valueHourOut > 24){
+  return alert('Hora de entrada ou saída não pode ser maior que 24hrs')
 
+}else if(valueHourIn < 0 || valueHourOut < 0){
+  return alert('Hora de entrada ou saída não pode ser menor que 00')
+
+}else if(valueMinIn < 0 || valueMinOut < 0){
+  return alert('Minuto de entrada ou saída não pode ser menor que 00')
+
+}
 
     // Envia os dados para o back-end
     const response = await api.post("projects", {
@@ -37,9 +50,11 @@ function App() {
 
     setCalculateHours([...calculateHours, project]);
 
-    //calculateHours.push('23:55')
-    //setCalculateHours([...calculateHours, ` Nova hora ${Date.now()}`] )
-    //console.log(calculateHours)
+  }
+
+  async function handleReset(){
+    setCalculateHours([])
+    await api.post("projects", {del: true});
   }
 
   return (
@@ -48,19 +63,23 @@ function App() {
 
      
         <h4>Digite a Hora de entrada</h4>
-        <label> Hora <input id="hourIn" type="number" min="0" max="23" name="HourIn"  /></label>:
+        <label> Hora <input id="hourIn" type="number" min="0" max="23" name="HourIn"  /></label>
         <label> Minuto <input id="minIn" type="number" min="0" max="59" name="MinIn" /></label>
         <h4>Digite a Hora de saída</h4>
-        <label> Hora <input id="hourOut" type="number" min="0" max="23" name="HourOut" /></label>:
-        <label> Minuto <input id="minOut" type="number" min="0" max="60" name="MinOut" /></label>
+        <label> Hora <input id="hourOut" type="number" min="0" max="23" name="HourOut" /></label>
+        <label> Minuto <input id="minOut" type="number" min="0" max="59" name="MinOut" /></label>
 
         <h4></h4>
 
       <button type="button" onClick={handleAddHours}>
         Cálcular horas
       </button>
+     <span> </span>
+      <button type="button" onClick={handleReset}>
+        Resetar valores
+      </button>
 
-      <h4>Resulatdo</h4>
+      <h4>Resulatdo:</h4>
 
 
       <ul>
