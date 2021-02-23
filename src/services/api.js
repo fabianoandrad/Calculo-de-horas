c6d67bc8cfaa0c01672313d93de23d9c2352c0fb
fabianoandrad <affatecnologia@gmail.com>
2021-02-23 11:15:05 -0300

    alteração pa deploy no bundle

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://calculatorbd-com.umbler.net",
+  baseURL: "https://calculo-de-horas-back-end.vercel.app",
 });
 
 export default api;
